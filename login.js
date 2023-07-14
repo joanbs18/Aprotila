0a29db27d2ee7d17186572e8b8aec446426cbb42
toro2002 <110430626+toro2002@users.noreply.github.com>
2023-07-14 11:40:19 -0600

    validaciones del login
    las validaciones creo que ya están solo que no se muestra el alert

--- login.js
+++ login.js
@@ -1,25 +1,18 @@
+function validarlogin() {
+  var username = document.getElementById('username').value;
+  var password = document.getElementById('password').value;
 
-document.querySelector('form').addEventListener('submit', function(event) {
-    var usernameInput = document.getElementById('username');
-    var passwordInput = document.getElementById('password');
-    
-    var username = usernameInput.value;
-    
-    if (!/^\d+$/.test(username)) {
-      alert('El usuario solo puede contener números.');
-      event.preventDefault(); 
-      return;
-    }
+  // Validar que el usuario solo contenga números
+  if (!/^\d+$/.test(username)) {
+    alert('El usuario solo debe contener números.');
+    return false;
+  }
 
-    if (username.length < 9) {
-      alert('El usuario debe tener al menos 9 caracteres.');
-      event.preventDefault(); 
-      return;
-    }
-    var password = passwordInput.value;
-    if (password.length < 8) {
-      alert('La contraseña debe tener al menos 8 caracteres.');
-      event.preventDefault(); 
-      return;
-    }
-});
+  // Validar que la contraseña tenga al menos 8 caracteres
+  if (password.length < 8) {
+    alert('La contraseña debe tener al menos 8 caracteres.');
+    return false;
+  }
+
+  return true; // Enviar el formulario si todas las validaciones pasan
+}
