65b986174abb4b1fd0b1159ceb29debc4fd0cbdd
Xavier Sánchez Villalobos <74043552+Xavi0927@users.noreply.github.com>
2023-07-25 16:29:23 -0600

    Se actualizo el login

--- login.js
+++ login.js
@@ -1,1 +1,43 @@
+function validarlogin() {
+  var username = document.getElementById('cedula').value;
+  var password = document.getElementById('contraseña').value;
 
+  // Validar que el usuario solo contenga números
+  if (!/^\d+$/.test(username)) {
+    alert('El usuario solo debe contener números.');
+    return false;
+  }
+
+  // Validar que la contraseña tenga al menos 8 caracteres
+  if (password.length < 8) {
+    alert('La contraseña debe tener al menos 8 caracteres.');
+    return false;
+  }
+
+  Usuario(); // Enviar el formulario si todas las validaciones pasan
+}
+
+//Xavier
+function Usuario(){
+    console.log("hola mundo")
+    cedula = document.getElementById("cedula").value;
+    contraseña = document.getElementById("contraseña").value;
+    url = `http://localhost:3000/usuarios?Cedula=${cedula}&Contraseña=${contraseña}`;
+  
+    fetch(url, {
+      method: "get",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((resp) => resp.json())
+      .then((datos) => {
+        console.log(datos)
+        if (datos.mensaje === "Ingresado correctamente"){
+          window.location.href="/ControlVentas.html"
+        }else{
+          alert("Cedula o Contraseña invalida")
+        }
+      })
+      .catch((err) => console.log(err));
+  }
